2f5f34be1e14aed6b6206e66d04ce6084743200c
Dingnan Zhou <Dingnan@dingnantekiair.bc.edu>
2017-09-12 14:44:02 -0400

    Final

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -7,17 +7,64 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    //MARK: -Properties
+    @IBOutlet weak var awesomeimage: UIImageView!
     @IBOutlet weak var messageLabel: UILabel!
-    var index = 0
+    @IBOutlet weak var soundSwitch: UISwitch!
+    var awesomePlayer = AVAudioPlayer()
+    var index = -1
+    var imageNumber = -1
+    var soundNumber = -1
+    let numberOfImages = 5
+    let numberOfSounds = 6
+    var soundName = ""
     
+    //FIXME: -Please repair this error
+    //TODO: -Update viewDidLoad
     //This code executes when the view controller loads
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    //MARK: - My Own Functions
+    func playSound(soundName: String, audioPlayer: inout AVAudioPlayer){
+        // Can we load in the file soundName
+        if let sound = NSDataAsset(name:soundName){
+            // check if sound.data is a sound file
+            do{
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                //if sound.data is not a valid audio file
+                print("ERROR: data in \(soundName) couldn't be played")
+            }
+        } else {
+            //if reading in the NSDataAsset didn't work, tell the user/report the error.
+            print("ERROR: file\(soundName) didn't load")
+        }
+        
+    }
 
+    func nonRepeatingRandom(lastNumber: Int, maxValue: Int) ->Int {
+        var newIndex = -1
+        repeat {
+            newIndex = Int(arc4random_uniform(UInt32(maxValue)))
+        } while index == newIndex
+        return newIndex
+    }
+    //MARK: -Actions
+    @IBAction func soundSwtichPressed(_ sender: UISwitch) {
+        if !soundSwitch.isOn {
+            if soundNumber != -1 {
+            //stop playing
+            awesomePlayer.stop()
+            }
+            
+        }
+    }
+    
     @IBAction func messageButtonPressed(_ sender: UIButton) {
 
         let messages = ["You Are Fantastic!",
@@ -29,42 +76,27 @@ class ViewController: UIViewController {
                         "I can't wait to see your app",
                         "Fabulous? That's You!"]
         
-        var newIndex = -1
-        
-        repeat {
-            newIndex = Int(arc4random_uniform(UInt32(messages.count)))
-        } while index == newIndex
-        
-        index = newIndex
+        // Show a message
+        index = nonRepeatingRandom(lastNumber: index, maxValue: messages.count)
         messageLabel.text = messages[index]
         
-//        var randomIndex =
-//            Int(arc4random_uniform(UInt32(messages.count)))
-//        messageLabel.text = messages[randomIndex]
-//        messageLabel.text = messages[index]
-//        index = index + 1
-//
-//        if index == messages.count {
-//            index = 0
+        //Show an image
+        awesomeimage.isHidden = false
+        
+            imageNumber = nonRepeatingRandom(lastNumber: imageNumber, maxValue: numberOfImages)
+        awesomeimage.image = UIImage(named: "image\(imageNumber)")
+        
+        if soundSwitch.isOn {
+            //Get a random number to use in our soundName file
+            soundNumber = nonRepeatingRandom(lastNumber: soundNumber, maxValue: numberOfSounds)
+            
+            //play a sound
+            soundName = "sound\(soundNumber)"
+            playSound(soundName: soundName, audioPlayer: &awesomePlayer)
         }
         
+        }
         
-        
-
-        
-        
-//        let message1 = "You Are Fantastic!!!!"
-//        let message2 = "You Are Great!"
-//        let message3 = "You Are Amazing!"
-//
-//        if messageLabel.text == message1 {
-//            messageLabel.text = message2
-//        }else if messageLabel.text == message2{
-//            messageLabel.text = message3
-//    } else {
-//        messageLabel.text = message1
-//    }
- 
 }
 
 
